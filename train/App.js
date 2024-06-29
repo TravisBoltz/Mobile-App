1c8879262f740b3e688205166e4be5ebe02b2007
TravisBoltz <fariduoubdamusah@gmail.com>
2024-06-29 12:16:42 +0000

    chore: Update App.js to center align content

--- train/App.js
+++ train/App.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "plum",
-    // alignItems: "center",
-    // justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
