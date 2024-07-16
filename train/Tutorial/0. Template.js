45b99dddecb6b30dcc1f0e67f8cef66ec683b4ed
FoM <fariduoubdamusah@gmail.com>
2024-07-16 17:07:14 +0000

    delete repo

--- train/Tutorial/0. Template.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
